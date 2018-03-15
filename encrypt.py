9e7a6879074b44495f0b9eca11923c52d6dc2a0b
C-Beck <C-Beck@users.noreply.github.com>
2018-03-15 19:26:04 -0400

    Added code to get seed

--- encrypt.py
+++ encrypt.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python
 
-#called as python encrypt.py <mode> <date offset> <file>
+#called as python encrypt.py <mode> <symbol> <date offset> <file>
 
 import sys
 from alpha_vantage.timeseries import TimeSeries
 import json
 import datetime
+import hashlib
 
-with open(sys.argv[3],'r') as file:
+with open(sys.argv[4],'r') as file:
 	plain=file.read()
 
-print plain
+#print plain
+
+ts = TimeSeries(key='KO7DO2WL5QRGHRM1')
+data,meta_data=ts.get_daily(symbol=sys.argv[2], outputsize='compact')
+
+seed = float(data[(datetime.date.today()+datetime.timedelta(int(sys.argv[3]))).isoformat()]['4. close'])
+
+print seed
